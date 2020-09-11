46994ada4afbd1192868691a8a2fdf5a39b5b7d5
Mark Petersen <markiepetersen@gmail.com>
2020-09-10 18:08:37 -0600

    continued working on R-RANSAC

--- ransac/matlab/test.m
+++ ransac/matlab/test.m
@@ -1,44 +1,68 @@
 
 
 
-x = 0.2;
+PD = 0.9;
+PG = 0.95;
+
+lT = 0.9;
+lF = 1.6;
+
+lo = 0;
+
+count = 0;
+
+for kk = 1:100
+ll = 0;
+for jj = 1:100
+
+    if rand(1) > PD*PG
+        mT = 0;
+    else
+        mT = 1;
+    end
+    
+%     mT = poissrnd(lT);
+    mF = poissrnd(lF);
+    
+m = mT*0+mF;
+
+    s = 0;
+    for ii = 0:m
+
+        s = s+ poisson(lT,ii)*poisson(lF, m-ii);
+
+    end
+    s;
+    % ln = log(s/poisson(lF,m));
+    ln = log(PD*PG/lF*m+1-PD*PG);
+
+    ll = ln+ll;
+
+end
+
+ll;
+p = 1 - 1/(1+exp(ll));
+
+if p < 0.5
+    count = count + 1;
+    
+end
+
+end
+
+count
+
+function p = poisson(lambda, k)
+
+p = exp(-lambda)*lambda^k/factorial(k);
+
+end
 
 
 
 
 
-[z,fval] = fmincon(@fun,x,1,1)
 
 
-
-
-
-%% Derivative test
-
-rng('default')
-
-P = eye(4)*2;
-xh = [1;1;3;2];
-x = [2;3;0;0];
-C = [1 0 0 0; 0 1 0 0];
-R = eye(2)*0.1;
-r = chol(R);
-
-v1 = r*randn(2,1);
-v2 = r*randn(2,1);
-
-y1 = C*x +v1;
-y2 = C*x +v2;
-
-z1 = y1 - C*xh;
-z2 = y2 - C*xh;
-
-Z = C'*inv(R)*z1 + C'*inv(R)*z2;
-
-Pi = inv(P) + C'*inv(R)*C*2
-xn = xh + inv(Pi)*Z
-
-
-inv(Pi)*inv(P)*xh + inv(Pi)*C'*inv(R)*(y1+y2)
 
 
