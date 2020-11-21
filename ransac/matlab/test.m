9957f9a8244d3e1d40a460e0fceab941d19d2b20
Mark Petersen <pmarke@byu.edu>
2020-11-21 10:01:55 -0700

    fixed some of the notes

--- ransac/matlab/test.m
+++ ransac/matlab/test.m
@@ -1,119 +1,29 @@
-%% Testing my model likelihood
+u1 = rand(1000,1);
+u2 = rand(1000,1);
 
-rng('default')
-PD = 0.9;
-PG = 0.95;
 
-lT = 0.9;
-lF = 1.6;
 
-lo = 0;
+a = sqrt(-2*log(u1)).*cos(2*pi*u2);
 
-count = 0;
-
-for kk = 1:100
-ll = 0;
-for jj = 1:10
-
-    if rand(1) > PD*PG
-        mT = 0;
-    else
-        mT = 1;
-    end
-    
-%     mT = poissrnd(lT);
-    mF = poissrnd(lF);
-    
-m = mT+mF;
-
-%     s = 0;
-%     for ii = 0:m
+% for ii = 1:length(a)
 % 
-%         s = s+ poisson(lT,ii)*poisson(lF, m-ii);
-% 
+%     b = rand(1,1);
+%     if (b <= 0.5)
+%         a(ii) = a(ii) *(-1);
 %     end
-%     s;
-%     % ln = log(s/poisson(lF,m));
-if m >0
-    ln = log(PD*PG/lF*m+1-PD*PG);
-else
-    ln = log(1-PD*PG);
-end
+%     
+% end
 
-    ll = ln+ll;
 
-end
 
-ll;
-p = 1 - 1/(1+exp(ll));
-% p = 1 - 1/(1+10^ll);
 
-if p < 0.5
-    count = count + 1;
-    
-end
+figure(1),clf;
+histogram(a,'Normalization','probability')
+
+
+
+function p = myNormal(x)
+
+1/sqrt(2*pi)*exp(-1/2*x^2);
 
 end
-
-count
-
-
-%%
-
-% n = 4;
-% PG = 0.8;
-% g = chi2inv(PG,n);
-% S = eye(3);
-% c = pi^(3/2)/gamma(n/2+1);
-% V = c*g^(n/2)*det(S)
-
-%%
-x = rand(1)*5;
-h = rand(1)*2;
-func((2*x+2*h))
-funcT(2*x,h)
-
-%%
-
-syms x y z th real
-assumeAlso(x^2+y^2+z^2 == th);
-
-a = (1-cos(th))/th^2;
-b = (th - sin(th))/th^3;
-
-t =  [1;0;0]+a*[0 -z y]' + b*[-z^2-y^2 x*y x*z]';
-simplify(t'*t)
-
-
-function y = func(x)
-
-y = 2*x^2 + x  +1;
-
-end
-
-function y = funcT(x,h)
-
-y = func(x) + 2*(4*x +1)*h + 4*2*h^2;
-
-end
-
-
-
-
-
-
-
-function p = poisson(lambda, k)
-
-p = exp(-lambda)*lambda^k/factorial(k);
-
-end
-
-
-
-
-
-
-
-
-
